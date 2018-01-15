4c33dfb6ba83711af2f7e51ff9b8858df76f8c66
Aditia Darmadi Jiwandono <aditiadarmadij@gmail.com>
2018-01-15 14:18:15 +0700

    Completed level 1

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 //Level 0 Manipulation Number
 console.log('------Level 0--------')
-var myNumber = 90
-let yourNumber = 40
-const hisNumber = 20
+var numberH = 90
+let numberI = 40
+const numberJ = 20
+const numberK = 
 
-console.log(myNumber)
-console.log(yourNumber)
-console.log(hisNumber)
+console.log(numberH)
+console.log(numberI)
+console.log(numberJ)
 
 //Level 1
+var a = 3;
+var k = 66;
+var l = 4;
 
+var addition = a + l;
+var substraction = k - l;
+var multiply = k / a;
+var divided = l * a;
+var modulo = k % l;
+
+console.log(addition);
+console.log(substraction);
+console.log(multiply);
+console.log(divided);
+console.log(modulo);
+
+//Level 2
 
