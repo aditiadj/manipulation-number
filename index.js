906d75a9cef6713e9a3f3742260d901a8282bf94
Aditia Darmadi Jiwandono <aditiadarmadij@gmail.com>
2018-01-15 14:23:18 +0700

    Just completed  level 0-1

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ console.log('------Level 0--------')
 var numberH = 90
 let numberI = 40
 const numberJ = 20
-const numberK = 
 
 console.log(numberH)
 console.log(numberI)
 console.log(numberJ)
 
 //Level 1
+console.log('------Level 1--------')
 var a = 3;
 var k = 66;
 var l = 4;
