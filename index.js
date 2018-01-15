c8e24dca4069a70e0c3d939c62e2e12e7c957882
Aditia Darmadi Jiwandono <aditiadarmadij@gmail.com>
2018-01-15 13:20:15 +0700

    Update level 0 manipulation number

---
+++ index.js
@@ -0,0 +1,13 @@
+//Level 0 Manipulation Number
+console.log('------Level 0--------')
+var myNumber = 90
+let yourNumber = 40
+const hisNumber = 20
+
+console.log(myNumber)
+console.log(yourNumber)
+console.log(hisNumber)
+
+//Level 1
+
+
